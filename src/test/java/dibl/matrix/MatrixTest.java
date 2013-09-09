021697c86c65c68c4defb6e8259a23e50f73d47e
Jo Pol <jo3.pol@gmail.com>
2013-09-09 08:49:39 +0200

    improved flipping but GeneratorTest.customMix4x4() still produces incorrect diagrams

--- src/test/java/dibl/matrix/MatrixTest.java
+++ src/test/java/dibl/matrix/MatrixTest.java
@@ -26,23 +26,24 @@ public class MatrixTest
 {
     private static MatrixTransformer<Transformer<String>> transformer;
     private static String[][] m = { {"1", "2", "3"}, {"4", "5", "6"}, {"7", "8", "9"}};
+    private static String[][] m4 = { {"a", "b", "c", "d"}, {"e", "f", "g", "h"}, {"i", "j", "k", "l"}, {"m", "n", "o", "p"}};
 
     @BeforeClass
     public static void init()
     {
         transformer = new MatrixTransformer<Transformer<String>>(new Transformer<String>()
         {
-            public String flipLeftRight(String o)
+            public String flipLeftRight(final String o)
             {
                 return o;
             }
 
-            public String flipBotomUp(String o)
+            public String flipBotomUp(final String o)
             {
                 return o;
             }
 
-            public String rotate180(String o)
+            public String rotate180(final String o)
             {
                 return o;
             }
@@ -52,42 +53,56 @@ public class MatrixTest
     @Test
     public void skewDown()
     {
-        String s = Arrays.deepToString(Extractor.skewDown(m));
+        final String s = Arrays.deepToString(Extractor.skewDown(m));
         assertThat(s, is("[[1, 5, 9], [4, 8, 3], [7, 2, 6]]"));
     }
 
     @Test
     public void skewUp()
     {
-        String s = Arrays.deepToString(Extractor.skewUp(m));
+        final String s = Arrays.deepToString(Extractor.skewUp(m));
         assertThat(s, is("[[1, 8, 6], [4, 2, 9], [7, 5, 3]]"));
     }
 
     @Test
     public void shift()
     {
-        String s = Arrays.deepToString(Extractor.shift(m, 1, 1));
+        final String s = Arrays.deepToString(Extractor.shift(m, 1, 1));
         assertThat(s, is("[[5, 6, 4], [8, 9, 7], [2, 3, 1]]"));
+    }
+
+    @Test
+    public void ne2sw()
+    {
+        final String s = Arrays.deepToString(transformer.flipNE2SW(m4));
+        assertThat(s, is("[[a, e, i, m], [b, f, j, n], [c, g, k, o], [d, h, l, p]]"));
+    }
+
+    @Test
+    public void nw2se()
+    {
+        final String s = Arrays.deepToString(transformer.flipNW2SE(m4));
+        assertThat(s, is("[[p, l, h, d], [o, k, g, c], [n, j, f, b], [m, i, e, a]]"));
     }
 
     @Test
     public void flipBottomUp()
     {
-        String s = Arrays.deepToString(transformer.flipBottomUp(m));
+        final String s = Arrays.deepToString(transformer.flipBottomUp(m));
         assertThat(s, is("[[7, 8, 9], [4, 5, 6], [1, 2, 3]]"));
     }
 
     @Test
     public void flipLeftRight()
     {
-        String s = Arrays.deepToString(transformer.flipLeftRight(m));
+        final String s = Arrays.deepToString(transformer.flipLeftRight(m));
         assertThat(s, is("[[3, 2, 1], [6, 5, 4], [9, 8, 7]]"));
     }
 
     @Test
     public void rotate180()
     {
-        String s = Arrays.deepToString(transformer.rotate180(m));
+        final String s = Arrays.deepToString(transformer.rotate180(m));
         assertThat(s, is("[[9, 8, 7], [6, 5, 4], [3, 2, 1]]"));
     }
 }
