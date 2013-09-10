46fd1c90388263a48912ce3b741813ffb152757f
Jo Pol <jo3.pol@gmail.com>
2013-09-10 17:16:22 +0200

    flipping seems OK, custom mix of stitches not

--- src/test/java/dibl/p2t/GeneratorTest.java
+++ src/test/java/dibl/p2t/GeneratorTest.java
@@ -21,6 +21,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
 
+import org.jdom2.JDOMException;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Ignore;
@@ -70,16 +71,14 @@ public class GeneratorTest
     {
         final String[][] stiches = { {"tcptc", "tc", "tcptc", "tc"}, {"tc", "tcptc", "tc", "tcptc"}, {"tcptc", "tc", "tcptc", "tc"},
                 {"tc", "tcptc", "tc", "tcptc"}};
-        final PairTraversalPattern[] variants = loadVariants("4x4_522.txt");
-        Generator.generateCustomMix(stiches, "target/custom-mix4x4", variants);
+        Generator.generateCustomMix(stiches, "target/custom-mix4x4", loadVariants("4x4_522.txt"));
     }
 
     @Test
     public void customMix3x3() throws Exception
     {
         final String[][] stiches = { {"tc", "tcptc", "tc"}, {"tc", "tcptc", "tc"}, {"tcptc", "tc", "tcptc"}};
-        final PairTraversalPattern[] variants = loadVariants("3x3_1.txt");
-        Generator.generateCustomMix(stiches, "target/custom-mix3x3", variants);
+        Generator.generateCustomMix(stiches, "target/custom-mix3x3", loadVariants("3x3_1.txt"));
     }
 
     @Ignore
@@ -90,25 +89,41 @@ public class GeneratorTest
         new Generator(read("4x4_522.txt")).variations("tc", "tcptcp");
     }
 
+    @Ignore // do not break the build with uncommitted input
     @Test
     public void allInputFiles_tcptc() throws Exception
     {
-        String outputFolder = "target/all/";
-        new File(outputFolder).mkdirs();
-        int i = 0;
-        for (final File file : new File(INPUT_FOLDER).listFiles())
+        new File("target/all").mkdirs();
+        int[] files3x3 = {1, 2, 3, 4, 5, 6, 7};
+        int[] files4x4 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 16, 17, 18, 19, 20, 22, 23, 24, 25, 26, 27, 28, 29, 33, 34, 35, 36, 40, 41, 42, 43,
+                44, 45, 47, 48, 49, 50, 51, 52, 54, 55, 56, 58, 60, 61, 65, 67, 70, 74, 75, 79, 81, 86, 87, 88, 92, 96, 98, 101, 102, 106, 109, 112, 113, 117,
+                118, 122, 125, 131, 132, 133, 134, 135, 136, 137, 138, 144, 145, 146, 147, 149, 151, 153, 158, 162, 165, 166, 167, 169, 170, 171, 172, 174,
+                176, 177, 178, 179, 180, 182, 185, 186, 189, 190, 191, 194, 195, 196, 197, 198, 199, 200, 201, 205, 206, 207, 209, 210, 211, 212, 221, 222,
+                227, 231, 232, 242, 244, 245, 251, 254, 256, 265, 281, 291, 325, 335, 336, 338, 342, 343, 344, 345, 346, 349, 350, 351, 352, 353, 355, 356,
+                360, 364, 365, 366, 367, 372, 374, 382, 390, 392, 393, 397, 407, 408, 413, 414, 417, 418, 419, 420, 421, 423, 426, 427, 428, 429, 434, 440,
+                444, 446, 447, 448, 456, 466, 496, 497, 499, 500, 502, 509, 510, 512, 515, 516, 517, 518, 520, 521, 522, 558, 559, 569, 575, 601, 629, 656,
+                740,};
+
+        for (int i : files3x3)
+            gen("3x3_" + i);
+        for (int i : files4x4)
+            gen("4x4_" + i);
+    }
+
+    private void gen(final String file) throws FileNotFoundException, IOException, JDOMException
+    {
+        PairTraversalPattern[] variations = loadVariants(file + ".txt");
+        for (int i = 0; i < variations.length; i++)
         {
-            final FileInputStream in = new FileInputStream(file);
-            final PrintStream out = new PrintStream(new FileOutputStream(outputFolder + file.getName() + ".svg"));
-            Generator.generate(new PairTraversalPattern(in), out, "tcptc");
-            if (++i % 20 == 0)
-                System.out.print(" " + i);
+            final PrintStream out = new PrintStream(new FileOutputStream("target/all/" + file + "_" + i + ".svg"));
+            Generator.generate(read(file + ".txt"), out, "tcptc");
         }
+        System.out.print("+");
     }
 
     private PairTraversalPattern[] loadVariants(final String string) throws IOException, FileNotFoundException
     {
-        return new PatternProperties(read(string)).toVariationsArray();
+        return new PatternProperties(read(string)).toPatterns();
     }
 
     private PairTraversalPattern read(final String string) throws IOException, FileNotFoundException
