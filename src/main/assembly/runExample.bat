edd8df367d5ebb29fd86886d8cd7cc30f40db43b
joke.pol <jo3.pol@gmail.com>
2013-12-22 13:41:01 +0100

    improved error handling; windows command line example

---
+++ src/main/assembly/runExample.bat
@@ -0,0 +1,15 @@
+java -jar dibl-${pom.version}.jar 3;3^
+
+tcptc;tc;tcptc^
+
+tc;tcptc;tc^
+
+tcptc;tc;tcptc 3;3^
+
+(0,1,1,0,-1,-1);(1,0,1,0,-1,-1);(0,0,1,1,-1,-1)^
+
+(0,1,1,-1,-1,0);(-1,1,0,1,0,-1);(0,1,1,0,-1,-1)^
+
+(-1,1,1,0,-1,0);(0,0,0,0,0,0);(1,1,0,-1,0,-1)^
+
+< input/PairTraversal/diamond/3x3.svg > output.svg
