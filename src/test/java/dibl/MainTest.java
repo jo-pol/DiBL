847c8428503c0e6d7fa9f9612d3a538cc40b9cb1
Jo Pol <jo3.pol@gmail.com>
2013-12-22 20:44:28 +0100

    completed new command line interface
    - allowing jpg/png/tiff output
    - restored flip and rotate

--- src/test/java/dibl/MainTest.java
+++ src/test/java/dibl/MainTest.java
@@ -22,49 +22,74 @@ import java.io.PrintStream;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.powermock.reflect.Whitebox;
 
 public class MainTest
 {
+    private static final String DIAMOND_PATTERNS = "src/main/assembly/input/PairTraversal/diamond/";
     private static final String BRICK_PATTERNS = "src/main/assembly/input/PairTraversal/brick/";
+    private static final String STITCHES = "4;4\n" + "tcptc;tc;tcptc;tc\n" + "tc;tcptc;tc;tcptc\n" + "tcptc;tc;tcptc;tc\n" + "tc;tcptc;tc;tcptc\n";
     private PrintStream savedOut;
     private InputStream savedIn;
 
     @Test
     public void noArgs() throws Exception
     {
-        Whitebox.setInternalState(Main.class, "README", "src/main/assembly/README.txt");
         Main.main();
     }
 
     @Test
     public void inlineSingleArg() throws Exception
     {
-        System.setIn(new FileInputStream(BRICK_PATTERNS+"3x3.svg"));
-        System.setOut(new PrintStream(new FileOutputStream("target/main1.svg")));
-        Whitebox.setInternalState(Main.class, "README", "src/main/assembly/README.txt");
-        Main.main("3\t3\ntcptc\ttc\ttcptc\ntc\ttcptc\ttc\ntcptc\ttc\ttcptc");
+        System.setIn(new FileInputStream(DIAMOND_PATTERNS + "3x3.svg"));
+        System.setOut(new PrintStream(new FileOutputStream("target/main1.png")));
+        Main.main("-ext", "png", STITCHES);
     }
 
     @Test
     public void inlineTwoArgs() throws Exception
     {
-        System.setIn(new FileInputStream(BRICK_PATTERNS+"3x3.svg"));
+        System.setIn(new FileInputStream(DIAMOND_PATTERNS + "3x3.svg"));
         System.setOut(new PrintStream(new FileOutputStream("target/main2.svg")));
-        Whitebox.setInternalState(Main.class, "README", "src/main/assembly/README.txt");
-        Main.main("3\t3\ntcptc\ttc\ttcptc\ntc\ttcptc\ttc\ntcptc\ttc\ttcptc"//
-                , "3\t3\n" //
-                        + "(0,1,1,0,-1,-1)\t(1,0,1,0,-1,-1)\t(0,0,1,1,-1,-1)\n" //
-                        + "(0,1,1,-1,-1,0)\t(-1,1,0,1,0,-1)\t(0,1,1,0,-1,-1)\n" //
-                        + "(-1,1,1,0,-1,0)\t(0,0,0,0,0,0)\t(1,1,0,-1,0,-1) ");
+        Main.main(STITCHES, "3\t3\n" //
+                + "(0,1,1,0,-1,-1)\t(1,0,1,0,-1,-1)\t(0,0,1,1,-1,-1)\n" //
+                + "(0,1,1,-1,-1,0)\t(-1,1,0,1,0,-1)\t(0,1,1,0,-1,-1)\n" //
+                + "(-1,1,1,0,-1,0)\t(0,0,0,0,0,0)\t(1,1,0,-1,0,-1)");
     }
 
     @Test
-    public void customMix3x3new() throws Exception
+    public void hybrid() throws Exception
     {
-        System.setIn(new FileInputStream(BRICK_PATTERNS + "/3x3.svg"));
-        System.setOut(new PrintStream(new FileOutputStream("target/3x3.svg")));
-        Main.main(BRICK_PATTERNS + "3x3/3x3_1.txt", "target/3x3", "tc", "tcptc");
+        System.setIn(new FileInputStream(DIAMOND_PATTERNS + "4x4.svg"));
+        System.setOut(new PrintStream(new FileOutputStream("target/mainRotated.jpg")));
+        Main.main("-x", "-y", "-ext", "jpg", STITCHES, DIAMOND_PATTERNS + "4x4/4x4_2.txt");
+    }
+
+    @Test
+    public void hybridX() throws Exception
+    {
+        System.setIn(new FileInputStream(DIAMOND_PATTERNS + "3x3.svg"));
+        System.setOut(new PrintStream(new FileOutputStream("target/mainFlippedAlongX.jpg")));
+        Main.main("-x", "-ext", "jpg", STITCHES, DIAMOND_PATTERNS + "3x3/3x3_2.txt");
+    }
+
+    @Test
+    public void hybridY() throws Exception
+    {
+        System.setIn(new FileInputStream(DIAMOND_PATTERNS + "3x3.svg"));
+        System.setOut(new PrintStream(new FileOutputStream("target/mainFlippedAlongY.jpg")));
+        Main.main("-y", "-ext", "jpg", STITCHES, DIAMOND_PATTERNS + "3x3/3x3_2.txt");
+    }
+
+    @Test
+    public void brick() throws Exception
+    {
+        System.setIn(new FileInputStream(BRICK_PATTERNS + "4x4.svg"));
+        System.setOut(new PrintStream(new FileOutputStream("target/main3.svg")));
+        Main.main(STITCHES, "4;4\n" //
+                + "(-1,1,0,1,-1,0,0,0);(-1,1,0,0,1,-1,0,0);(-1,0,0,1,1,-1,0,0);(1,0,0,0,1,-1,0,-1)\n" //
+                + "(-1,1,0,0,1,-1,0,0);(-1,1,0,0,1,-1,0,0);(-1,1,0,0,1,-1,0,0);(-1,0,0,1,1,-1,0,0)\n" //
+                + "(1,1,0,0,-1,0,0,-1);(1,1,0,0,-1,-1,0,0);(1,1,0,0,-1,-1,0,0);(1,1,0,0,-1,-1,0,0)\n" //
+                + "(-1,1,0,1,-1,0,0,0);(-1,1,0,0,1,0,0,-1);(-1,1,0,0,1,-1,0,0);(1,0,0,0,1,-1,0,-1)\n");
     }
 
     @Before
