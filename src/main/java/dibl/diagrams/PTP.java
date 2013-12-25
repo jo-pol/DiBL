5b4c99619fdd832af1d30ee7688ef44b09407912
Jo Pol <jo3.pol@gmail.com>
2013-12-25 09:46:58 +0100

    attempt to flip brick tile based patterns
    odd rows require shifting

--- src/main/java/dibl/diagrams/PTP.java
+++ src/main/java/dibl/diagrams/PTP.java
@@ -28,4 +28,39 @@ public class PTP extends Matrix<LongTupleFlipper>
     {
         super(input, FLIPPER);
     }
+
+    @Override
+    public String[][] flipLeftRight()
+    {
+        // origin super required
+        // 1-2-3- 3-2-1- 3-2-1-
+        // -4-5-6 -6-5-4 -5-4-6
+        // 7-8-9- 9-8-7- 9-8-7-
+        return shiftOddRows(super.flipLeftRight());
+    }
+
+    @Override
+    public String[][] flipBottomUp()
+    {
+        String[][] m = super.flipBottomUp();
+        if (m[0].length % 2 == 0)
+            return shiftOddRows(m);
+        else
+            return m;
+    }
+
+    private String[][] shiftOddRows(String[][] m)
+    {
+        for (int r = 1; r < m.length; r += 2)
+        {
+            String saved = m[r][0];
+            int cols = m[r].length;
+            for (int c = 1; c < cols; c += 2)
+            {
+                m[r][c - 1] = m[r][c];
+            }
+            m[r][cols - 1] = saved;
+        }
+        return m;
+    }
 }
