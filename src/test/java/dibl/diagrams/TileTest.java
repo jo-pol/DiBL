f014839e1024e5bb01696698bebe73be0e37d4a6
Jo Pol <jo3.pol@gmail.com>
2013-12-28 15:55:07 +0100

    implemented torchon tile (some are sloppy)

--- src/test/java/dibl/diagrams/TileTest.java
+++ src/test/java/dibl/diagrams/TileTest.java
@@ -1,12 +1,11 @@
 package dibl.diagrams;
 
+import static dibl.diagrams.Tile.ColorCodedStitch.*;
+import static dibl.diagrams.Tile.ColorCodedStitch.tc;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
-import java.util.Arrays;
-
 import org.junit.Test;
-import static dibl.diagrams.Tile.ColorCodedStitch.*;
 
 public class TileTest
 {
@@ -17,40 +16,23 @@ public class TileTest
         final String expected = "<g inkscape:label=\"tc (0,1,0,0,1,0,-1,-1)\">\n" + //
                 "<path d=\"M 36,0 C 28,8  18,24 18,36\" " + attributes + " />\n" + //
                 "<path d=\"M 0,18 C 12,18  28,28 36,36\" " + attributes + " />\n" + //
-                "</g>";
+                "</g>\n";
         assertThat(new Tile(tc, "(0,1,0,0,1,0,-1,-1)").toString(), is(expected));
     }
 
     @Test
-    public void generate()
+    public void ctc()
     {
-        // -1,0,1,1,-1,0,0,0 -> 1221000
-        final int max = Integer.parseInt("22222222", 3);
-        for (int i = 0; i <= max; i++)
-        {
-            try
-            {
-                System.out.println(new Tile(tc, toTuple(Integer.toString(i, 3))));
-            }
-            catch (final IllegalArgumentException e)
-            {
-                // just skip
-            }
-            catch (final Exception e)
-            {
-                System.err.println(e.getMessage());
-            }
-        }
+        String string = Tile.generate(ctc);
+        System.out.println(string);
+        assertThat(string.split("\n").length, is(69 * 4));
     }
 
-    private String toTuple(final String s)
+    @Test
+    public void tcptc()
     {
-        final String padded = ("00000000" + s).substring(s.length());
-        final int[] items = new int[8];
-        for (int i = 0; i < 8; i++)
-        {
-            items[i] = Integer.parseInt(padded.substring(i, i + 1)) - 1;
-        }
-        return Arrays.toString(items).replace("[", "(").replace("]", ")");
+        String string = Tile.generate(tcptc);
+        System.out.println(string);
+        assertThat(string.split("\n").length, is(69 * 5));
     }
 }
