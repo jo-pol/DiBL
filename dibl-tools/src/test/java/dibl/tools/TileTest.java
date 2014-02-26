465b87771dbc153c356d790ee7a34707bc078cce
Jo Pol <jo3.pol@gmail.com>
2014-02-26 09:02:20 +0100

    generate preProtoTiles as complete document

--- dibl-tools/src/test/java/dibl/tools/TileTest.java
+++ dibl-tools/src/test/java/dibl/tools/TileTest.java
@@ -1,5 +1,8 @@
 package dibl.tools;
 
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+
 import org.junit.Test;
 
 public class TileTest
@@ -8,20 +11,29 @@ public class TileTest
     private static final String[] OUT = {"24,18 36,18", "-", "-", "-", "12,18 0,18", "8,28 0,36", "18,24 18,36", "28,28 36,36"};
 
     @Test
-    public void preProtoTiles()
+    public void preProtoTiles() throws Exception
     {
-        // outlines of the generated fat lines should be split and rearranged
-        // as sketched in http://wiki.dibl.googlecode.com/git/automated.svg
+        /*
+         * To examine the results in InkScape: align all objects (ctrl-shift-a) to the north and west,
+         * then spread them over rows and columns. Create outlines: select the node tool, select all
+         * paths and apply stroke-to-path (ctrl-alt-c). However, the paths are closed and don't have the
+         * desired number of nodes as sketched in
+         * https://code.google.com/p/dibl/wiki/NewStitches#Thread_diagrams
+         */
         String stroke = "000000;stroke-width:8";
+        final PrintWriter os = new PrintWriter(new FileOutputStream("target/preProtoTiles.svg"));
+        os.print(SvgDoc.HEAD);
         for (Tuple tuple : Tuple.list())
         {
             for (Stitch stitch : Stitch.values())
             {
                 final String line1 = new Line(stroke, IN[tuple.firstIn()], OUT[tuple.lastOut()]).toString();
                 final String line2 = new Line(stroke, IN[tuple.lastIn()], OUT[tuple.firstOut()]).toString();
-                System.out.println(new Tile(stitch, tuple, line1, line2));
+                os.println(new Tile(stitch, tuple, line1, line2));
             }
         }
+        os.print(SvgDoc.TAIL);
+        os.close();
     }
 
     @Test
