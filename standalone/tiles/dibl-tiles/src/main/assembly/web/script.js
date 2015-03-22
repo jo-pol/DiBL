778ffed2498877d04ad71ec634266a11e0ab5886
jo pol <jo-pol@users.noreply.github.com>
2015-03-22 15:25:23 +0100

    all three forms more or less working

--- standalone/tiles/dibl-tiles/src/main/assembly/web/script.js
+++ standalone/tiles/dibl-tiles/src/main/assembly/web/script.js
@@ -9,13 +9,11 @@
 // See the GNU General Public License for more details. A copy of the GNU General Public License is
 // available at <http://www.gnu.org/licenses/>.
 
-var command = "java -jar dibl-cmd-${project.version}.jar -ext "
-+ document.flandersConfig.ext.value
-;
+var command = "java -jar dibl-cmd-${project.version}.jar -ext ";
 
-function diagramTypeChanged (){
+function diagramTypeChanged (cfg){
 
-	var newDiagramType = document.diagramConfig.diagramType.value;
+	var newDiagramType = cfg.diagramType.value;
 	var newDisplayValue = newDiagramType == '-pair'? 'block' : 'none';
 
 	var elements = document.getElementsByClassName('pair');
@@ -24,13 +22,12 @@ function diagramTypeChanged (){
 	}
 }
 
-function setRangeMax() {
+function setRangeMax(cfg) {
 
-	d = document.diagramConfig.dimensions;
-	iHTML = d.options[d.selectedIndex].innerHTML;
-	f = document.diagramConfig.pattern;
-	f.value = 1;
-	f.max = parseInt(iHTML.split(":")[1]);
+	dim = cfg.dimensions;
+	iHTML = dim.options[dim.selectedIndex].innerHTML;
+	cfg.pattern.value = 1;
+	cfg.pattern.max = parseInt(iHTML.split(":")[1]);
 	document.getElementById('nr').innerHTML = 1;
 }
 
@@ -44,79 +41,80 @@ function showFlanders() {
 	// add to .bashrc on Windows something like:
 	// export PATH=$PATH:/C/Program\ Files/Java/jre6/bin
 
+	var cfg = document.flandersConfig;
 	var io = "< input/flanders.svg > diagram." 
-	+ document.flandersConfig.ext.value
+	+ cfg.ext.value
 	;
 	document.getElementById('sh').innerHTML
-	= command + " '3;2\n"
-	+ document.flandersConfig.A1.value + ";"
-	+ document.flandersConfig.B1.value + "\n"
-	+ document.flandersConfig.A2.value + ";"
-	+ document.flandersConfig.B2.value + "\n"
-	+ document.flandersConfig.A3.value + ";"
-	+ document.flandersConfig.B3.value + "' " + io
+	= command + cfg.ext.value + " '3;2\n"
+	+ cfg.A1.value + ";"
+	+ cfg.B1.value + "\n"
+	+ cfg.A2.value + ";"
+	+ cfg.B2.value + "\n"
+	+ cfg.A3.value + ";"
+	+ cfg.B3.value + "' " + io
 	;
 	document.getElementById('bat').innerHTML
-	= command + " 3;2^\r\n\r\n"
-	+ document.flandersConfig.A1.value + ";"
-	+ document.flandersConfig.B1.value + "^\r\n\r\n"
-	+ document.flandersConfig.A2.value + ";"
-	+ document.flandersConfig.B2.value + "^\r\n\r\n"
-	+ document.flandersConfig.A3.value + ";"
-	+ document.flandersConfig.B3.value + " " + io
+	= command + cfg.ext.value + " 3;2^\r\n\r\n"
+	+ cfg.A1.value + ";"
+	+ cfg.B1.value + "^\r\n\r\n"
+	+ cfg.A2.value + ";"
+	+ cfg.B2.value + "^\r\n\r\n"
+	+ cfg.A3.value + ";"
+	+ cfg.B3.value + " " + io
 	;
 }
 
-function showPairTraversal() {
+function showPairTraversal(cfg) {
 
 	// add to .bashrc on Windows something like:
 	// export PATH=$PATH:/C/Program\ Files/Java/jre6/bin
 
 	var templatePath = " input/PairTraversal/"
-	+ document.diagramConfig.traversalType.value + "/"
-	+ document.diagramConfig.dimensions.value
+	+ cfg.traversalType.value + "/"
+	+ cfg.dimensions.value
 	;
 	var io = " <" + templatePath 
-	+ document.diagramConfig.diagramType.value  + ".svg > diagram."
-	+ document.diagramConfig.ext.value
+	+ cfg.diagramType.value  + ".svg > diagram."
+	+ cfg.ext.value
 	;
 	var pattern = templatePath + "/"
-	+ document.diagramConfig.dimensions.value + "_"
-	+ document.diagramConfig.pattern.value + ".txt"
+	+ cfg.dimensions.value + "_"
+	+ cfg.pattern.value + ".txt"
 	;
 	var r1 
-	= document.diagramConfig.A1.value + ";"
-	+ document.diagramConfig.B1.value + ";"
-	+ document.diagramConfig.C1.value + ";"
-	+ document.diagramConfig.D1.value
+	= cfg.A1.value + ";"
+	+ cfg.B1.value + ";"
+	+ cfg.C1.value + ";"
+	+ cfg.D1.value
 	;
 	var r2
-	= document.diagramConfig.A2.value + ";"
-	+ document.diagramConfig.B2.value + ";"
-	+ document.diagramConfig.C2.value + ";"
-	+ document.diagramConfig.D2.value
+	= cfg.A2.value + ";"
+	+ cfg.B2.value + ";"
+	+ cfg.C2.value + ";"
+	+ cfg.D2.value
 	;
 	var r3
-	= document.diagramConfig.A3.value + ";"
-	+ document.diagramConfig.B3.value + ";"
-	+ document.diagramConfig.C3.value + ";"
-	+ document.diagramConfig.D3.value
+	= cfg.A3.value + ";"
+	+ cfg.B3.value + ";"
+	+ cfg.C3.value + ";"
+	+ cfg.D3.value
 	;
 	var r4
-	= document.diagramConfig.A4.value + ";"
-	+ document.diagramConfig.B4.value + ";"
-	+ document.diagramConfig.C4.value + ";"
-	+ document.diagramConfig.D4.value
+	= cfg.A4.value + ";"
+	+ cfg.B4.value + ";"
+	+ cfg.C4.value + ";"
+	+ cfg.D4.value
 	;
 	document.getElementById('sh').innerHTML
-	= command + document.diagramConfig.options.value + " '4;4\n"
+	= command + cfg.ext.value + " "  + cfg.options.value + " '4;4\n"
 	+ r1 + "\n"
 	+ r2 + "\n"
 	+ r3 + "\n"
 	+ r4 + "'" + pattern + io
 	;
 	document.getElementById('bat').innerHTML
-	= command + document.diagramConfig.options.value + " 4;4^\r\n\r\n"
+	= command + cfg.ext.value + " " + cfg.options.value + " 4;4^\r\n\r\n"
 	+ r1 + "^\r\n\r\n"
 	+ r2 + "^\r\n\r\n"
 	+ r3 + "^\r\n\r\n"
