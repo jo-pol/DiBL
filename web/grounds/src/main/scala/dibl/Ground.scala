0c79e99ab10219f557f3625c03e2c8e3bb55542d
jo pol <jo-pol@users.noreply.github.com>
2015-06-10 17:10:19 +0200

    exploring matrices

--- web/grounds/src/main/scala/dibl/Ground.scala
+++ web/grounds/src/main/scala/dibl/Ground.scala
@@ -20,36 +20,51 @@ import org.scalajs.dom
 import org.scalajs.dom.html.Document
 import scala.scalajs.js.annotation.JSExport
 import scala.collection.mutable
+import collection.immutable.HashMap
 
 @JSExport
 object Ground {
-  type Tuple = String
-  type Matrix = Array[Array[String]]
-  type Matrices = Array[Matrix]
-  type MatrixMap = mutable.HashMap[String, Matrices]
+  val R = Array
+  val M = Array
+  val tupleMatrices = Array( // diagonal 3x3
+    M(R("0,1,1,0,-1,-1","1,0,1,0,-1,-1","0,0,1,1,-1,-1"),R("0,1,1,-1,-1,0","-1,1,0,1,0,-1","0,1,1,0,-1,-1"),R("-1,1,1,0,-1,0","0,0,0,0,0,0","1,1,0,-1,0,-1")),
+    M(R("1,0,1,0,-1,-1","1,0,1,0,-1,-1","0,0,1,1,-1,-1"),R("0,1,1,-1,-1,0","-1,1,0,1,0,-1","0,1,1,-1,0,-1"),R("-1,1,1,0,0,-1","1,0,1,-1,0,-1","1,0,1,-1,0,-1")),
+    M(R("1,0,1,0,-1,-1","0,0,1,1,-1,-1","1,0,1,0,-1,-1"),R("-1,1,0,1,-1,0","0,1,0,1,-1,-1","0,1,1,-1,-1,0"),R("-1,1,1,-1,0,0","1,1,0,-1,0,-1","-1,1,1,0,0,-1")),
+    M(R("1,0,1,0,-1,-1","1,0,1,-1,0,-1","0,0,1,1,-1,-1"),R("1,1,0,0,-1,-1","-1,0,1,1,0,-1","0,1,1,-1,-1,0"),R("-1,1,1,0,0,-1","1,0,1,-1,0,-1","0,1,1,-1,0,-1")),
+    M(R("0,0,1,1,-1,-1","-1,1,1,-1,0,0","1,1,0,0,-1,-1"),R("1,1,0,0,-1,-1","0,0,1,1,-1,-1","-1,1,1,-1,0,0"),R("-1,1,1,-1,0,0","1,1,0,0,-1,-1","0,0,1,1,-1,-1")),
+    M(R("1,0,1,0,-1,-1","-1,0,1,1,-1,0","1,1,0,0,-1,-1"),R("-1,1,1,0,-1,0","0,1,0,1,-1,-1","0,1,1,-1,0,-1"),R("-1,1,1,-1,0,0","1,1,0,-1,0,-1","0,0,1,1,-1,-1")),
+    M(R("1,0,1,0,-1,-1","-1,1,1,0,0,-1","1,0,1,0,-1,-1"),R("0,1,0,1,-1,-1","0,0,1,1,-1,-1","0,1,1,-1,-1,0"),R("-1,1,1,-1,0,0","1,1,0,-1,-1,0","-1,1,0,1,0,-1"))
+  )
 
   val xpathStitches = "//g[@inkscape:label='pile']/g[@inkscape:label]"
   val xpathCells = "//g[@inkscape:label='base tile']/use[@inkscape:label]"
 
   @JSExport
   def main(document: Document): Unit = {
-    document.getElementById("demo").innerHTML += " it's me "
     val s = Settings (document.documentURI)
+    
+    // values from the HTML-form alias URI-query (relying on the defaults for now)
     val template: String = s.getArg("template","diagonal-3x3-thread")
     val pattern: Int = s.getArg("pattern","0").toInt
-    val matrixKey: String = template.replace("-thread","").replace("-pair","")
-    //val matrix: Matrix = matricesOfTuples.get(matrixKey).get(pattern)
+    val stitches = HashMap(
+        "A1" -> "tc", "A2" -> "tc", "A3" -> "tc", 
+        "B1" -> "tc", "B2" -> "tc", "B3" -> "tc", 
+        "C1" -> "tc", "C2" -> "tc", "C3" -> "tc")
+
+    val tuples: Array[Array[String]] = tupleMatrices(pattern)
+    // TODO yield tuples onto stitches to produce labels: "tc (0,1,1,0,-1,-1)"
+    val labels = stitches
 
     val xhr = new dom.XMLHttpRequest()
     xhr.open("GET", s"http://jo-pol.github.io/DiBL/grounds/templates/${template}.svg")
     xhr.onload = (e: dom.Event) => {
         if (xhr.status == 200) {
-          document.write( replaceStitches(xhr.responseText, s) )
+          document.write( replaceStitches(xhr.responseText, labels) )
         }
     }
     xhr.send()
   }
-  def replaceStitches(svg:String, s: Settings): String = {
+  def replaceStitches(svg:String, labels: HashMap[String,String]): String = {
       //val stitches =...foreach yield
       //{ node => (node.attribute("inkscape:label") -> node.attribute("id")) }
       //val cells =...foreach yield
