c5a957698b18e4fd5730f47d8e0face99bed8ce1
jo pol <jo-pol@users.noreply.github.com>
2015-06-10 22:12:33 +0200

    clean up of the html, scaladoc, TODO's

--- web/grounds/build.sbt
+++ web/grounds/build.sbt
@@ -12,7 +12,6 @@ scalaVersion := "2.11.5"
 
 libraryDependencies += "org.scala-js" %%% "scalajs-dom" % "0.8.0"
 
-
 bootSnippet := "dibl.Ground().main(document, matrices);"
 
 updateBrowsers <<= updateBrowsers.triggeredBy(fastOptJS in Compile)
--- web/grounds/src/main/scala/dibl/Ground.scala
+++ web/grounds/src/main/scala/dibl/Ground.scala
@@ -19,56 +19,97 @@ import java.net.URI
 import org.scalajs.dom
 import org.scalajs.dom.html.Document
 import scala.scalajs.js.annotation.JSExport
-import scala.collection.mutable
 import collection.immutable.HashMap
 
 @JSExport
 object Ground {
-  val R = Array
-  val M = Array
-  val tupleMatrices = Array( // diagonal 3x3
+  val R = Array // row
+  val M = Array // matrix
+  /** See https://github.com/jo-pol/DiBL/wiki/Input-Files */
+  val matrixMap = HashMap(
+    "diagonal-3x3" -> Array(
     M(R("0,1,1,0,-1,-1","1,0,1,0,-1,-1","0,0,1,1,-1,-1"),R("0,1,1,-1,-1,0","-1,1,0,1,0,-1","0,1,1,0,-1,-1"),R("-1,1,1,0,-1,0","0,0,0,0,0,0","1,1,0,-1,0,-1")),
     M(R("1,0,1,0,-1,-1","1,0,1,0,-1,-1","0,0,1,1,-1,-1"),R("0,1,1,-1,-1,0","-1,1,0,1,0,-1","0,1,1,-1,0,-1"),R("-1,1,1,0,0,-1","1,0,1,-1,0,-1","1,0,1,-1,0,-1")),
     M(R("1,0,1,0,-1,-1","0,0,1,1,-1,-1","1,0,1,0,-1,-1"),R("-1,1,0,1,-1,0","0,1,0,1,-1,-1","0,1,1,-1,-1,0"),R("-1,1,1,-1,0,0","1,1,0,-1,0,-1","-1,1,1,0,0,-1")),
     M(R("1,0,1,0,-1,-1","1,0,1,-1,0,-1","0,0,1,1,-1,-1"),R("1,1,0,0,-1,-1","-1,0,1,1,0,-1","0,1,1,-1,-1,0"),R("-1,1,1,0,0,-1","1,0,1,-1,0,-1","0,1,1,-1,0,-1")),
     M(R("0,0,1,1,-1,-1","-1,1,1,-1,0,0","1,1,0,0,-1,-1"),R("1,1,0,0,-1,-1","0,0,1,1,-1,-1","-1,1,1,-1,0,0"),R("-1,1,1,-1,0,0","1,1,0,0,-1,-1","0,0,1,1,-1,-1")),
     M(R("1,0,1,0,-1,-1","-1,0,1,1,-1,0","1,1,0,0,-1,-1"),R("-1,1,1,0,-1,0","0,1,0,1,-1,-1","0,1,1,-1,0,-1"),R("-1,1,1,-1,0,0","1,1,0,-1,0,-1","0,0,1,1,-1,-1")),
-    M(R("1,0,1,0,-1,-1","-1,1,1,0,0,-1","1,0,1,0,-1,-1"),R("0,1,0,1,-1,-1","0,0,1,1,-1,-1","0,1,1,-1,-1,0"),R("-1,1,1,-1,0,0","1,1,0,-1,-1,0","-1,1,0,1,0,-1"))
+    M(R("1,0,1,0,-1,-1","-1,1,1,0,0,-1","1,0,1,0,-1,-1"),R("0,1,0,1,-1,-1","0,0,1,1,-1,-1","0,1,1,-1,-1,0"),R("-1,1,1,-1,0,0","1,1,0,-1,-1,0","-1,1,0,1,0,-1")))
+    ,"brick-3x3" -> Array(
+    M(R("-1,1,0,1,-1,0,0,0","0,0,0,1,1,-1,0,-1","1,1,0,0,0,-1,0,-1"),R("1,1,0,0,0,-1,0,-1","-1,1,0,1,-1,0,0,0","0,0,0,1,1,-1,0,-1"),R("-1,1,0,1,-1,0,0,0","0,0,0,1,1,-1,0,-1","1,1,0,0,0,-1,0,-1")),
+    M(R("-1,1,0,1,-1,0,0,0","0,1,0,0,1,-1,0,-1","1,1,0,0,0,-1,0,-1"),R("1,1,0,0,-1,0,0,-1","0,1,0,1,-1,0,0,-1","1,0,0,1,0,-1,0,-1"),R("-1,0,0,1,1,-1,0,0","0,0,0,1,1,-1,0,-1","-1,1,0,1,0,-1,0,0")),
+    M(R("-1,1,0,1,-1,0,0,0","0,1,0,0,1,-1,0,-1","1,1,0,0,0,-1,0,-1"),R("0,1,0,0,1,-1,0,-1","-1,1,0,1,0,0,0,-1","-1,0,0,1,1,0,0,-1"),R("0,1,0,1,-1,-1,0,0","1,0,0,1,0,-1,0,-1","1,0,0,1,-1,-1,0,0")),
+    M(R("-1,1,0,1,0,-1,0,0","-1,1,0,0,1,-1,0,0","0,1,0,0,1,-1,0,-1"),R("-1,1,0,0,1,-1,0,0","0,1,0,0,1,-1,0,-1","-1,1,0,1,0,0,0,-1"),R("0,1,0,1,-1,-1,0,0","1,1,0,0,0,-1,0,-1","1,0,0,1,-1,-1,0,0")),
+    M(R("-1,1,0,1,-1,0,0,0","0,1,0,0,1,-1,0,-1","1,0,0,1,0,-1,0,-1"),R("1,1,0,0,-1,0,0,-1","0,1,0,1,-1,-1,0,0","1,0,0,1,0,-1,0,-1"),R("0,0,0,1,1,-1,0,-1","-1,1,0,1,0,0,0,-1","-1,1,0,0,1,-1,0,0")),
+    M(R("-1,1,0,1,0,-1,0,0","-1,1,0,0,1,-1,0,0","0,0,0,1,1,-1,0,-1"),R("1,1,0,0,0,-1,0,-1","1,1,0,0,-1,-1,0,0","0,1,0,1,-1,-1,0,0"),R("0,1,0,0,1,-1,0,-1","-1,1,0,1,0,0,0,-1","-1,1,0,0,1,-1,0,0")),
+    M(R("-1,1,0,1,0,-1,0,0","-1,1,0,0,1,0,0,-1","0,0,0,1,1,-1,0,-1"),R("0,0,0,0,0,0,0,0","0,1,0,1,0,-1,0,-1","0,1,0,1,0,-1,0,-1"),R("1,0,0,1,0,-1,0,-1","1,1,0,0,-1,0,0,-1","0,1,0,1,-1,-1,0,0")),
+    M(R("0,1,0,1,0,-1,0,-1","0,1,0,1,0,-1,0,-1","0,1,0,1,0,-1,0,-1"),R("0,1,0,1,0,-1,0,-1","0,1,0,1,0,-1,0,-1","0,1,0,1,0,-1,0,-1"),R("0,1,0,1,0,-1,0,-1","0,1,0,1,0,-1,0,-1","0,1,0,1,0,-1,0,-1")))
   )
-
-  val xpathStitches = "//g[@inkscape:label='pile']/g[@inkscape:label]"
-  val xpathCells = "//g[@inkscape:label='base tile']/use[@inkscape:label]"
 
   @JSExport
   def main(document: Document): Unit = {
+    document.getElementById("message").innerHTML += " loading diagram... "
     val s = Settings (document.documentURI)
     
-    // values from the HTML-form alias URI-query (relying on the defaults for now)
-    val template: String = s.getArg("template","diagonal-3x3-thread")
-    val pattern: Int = s.getArg("pattern","0").toInt
-    val stitches = HashMap(
+    // values from the HTML-form alias URI-query (for now relying on defaults)
+    val template: String = s.getArg("template", "diagonal-3x3-thread")
+    val pattern: Int = s.getArg("pattern", "0").toInt
+    val stitches = HashMap( // A1-C3 are indexes for a matrix cell
         "A1" -> "tc", "A2" -> "tc", "A3" -> "tc", 
         "B1" -> "tc", "B2" -> "tc", "B3" -> "tc", 
         "C1" -> "tc", "C2" -> "tc", "C3" -> "tc")
 
-    val tuples: Array[Array[String]] = tupleMatrices(pattern)
+    val url = s"http://jo-pol.github.io/DiBL/grounds/templates/${template}.svg"
+    val tupleMatrix = matrixMap.get(template.replace("-thread","").replace("-pair","")).get(pattern)
     // TODO yield tuples onto stitches to produce labels: "tc (0,1,1,0,-1,-1)"
     val labels = stitches
 
+    // TODO perhaps replace with https://lihaoyi.github.io/hands-on-scala-js/#dom.extensions 
     val xhr = new dom.XMLHttpRequest()
-    xhr.open("GET", s"http://jo-pol.github.io/DiBL/grounds/templates/${template}.svg")
+    xhr.open("GET", url)
     xhr.onload = (e: dom.Event) => {
         if (xhr.status == 200) {
+          document.getElementById("message").innerHTML += " replacing stitches... "
           document.write( replaceStitches(xhr.responseText, labels) )
+          // TODO stop the busy icon of the browser
         }
     }
     xhr.send()
   }
-  def replaceStitches(svg:String, labels: HashMap[String,String]): String = {
-      //val stitches =...foreach yield
-      //{ node => (node.attribute("inkscape:label") -> node.attribute("id")) }
-      //val cells =...foreach yield
-      //{ node => (node.attribute("inkscape:label") -> node) }
-      svg
+  
+  /** Replaces stitches in an SVG template.
+    *
+    * An extract of a template:
+    * {{{
+    *    <g inkscape:label="base tile">
+    *      <use inkscape:label="A1" xlink:href="#g123"></use>
+    *      <use inkscape:label="A2" xlink:href="#g456"></use>
+    *      ...
+    *    </g>
+    *    <g inkscape:label="pile">
+    *      <g id="g123" inkscape:label="tc (0,1,1,0,-1,-1)">...</g>
+    *      <g id="g456" inkscape:label="tc (1,0,1,0,-1,-1)">...</g>
+    *      ...
+    *    </g>
+    * }}}
+    * For each cell (A1-..) in the base tile, look up the new label in the pile.
+    * This results in an ID. This ID becomes the new values of the href attribute in the base tile. 
+    */
+  def replaceStitches(svg:String, newLabels: HashMap[String,String]): String = {
+
+    val xpathStitches = "//g[@inkscape:label='pile']/g[@inkscape:label]"
+    val xpathCells = "//g[@inkscape:label='base tile']/use[@inkscape:label]"
+
+    // TODO fix linking errors or use something else than:
+    // libraryDependencies += "org.scala-lang.modules" %% "scala-xml" % "1.0.2"
+    
+    // scala.xml.XML.loadString(svg)
+    
+    // val stitches =...foreach yield
+    // { node => (node.attribute("inkscape:label") -> node.attribute("id")) }
+    // for each cell:
+    //   newLabel = newLabels.get(node.attribute("inkscape:label"))
+    //   node.attribute("xlink:href") = "#" + stitches.get(newLabel)
+    svg
   }
 }
