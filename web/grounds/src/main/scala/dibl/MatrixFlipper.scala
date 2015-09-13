4ed1935989e14da63ab44226bdec14964dc2503d
jo pol <jo-pol@users.noreply.github.com>
2015-09-13 19:40:56 +0200

    refactored flipping for testing purposes
    the new test might fail but sbt doesn't execute them anyhow

--- web/grounds/src/main/scala/dibl/MatrixFlipper.scala
+++ web/grounds/src/main/scala/dibl/MatrixFlipper.scala
@@ -16,8 +16,8 @@
 
 package dibl
 
-/** @param flipper object that flips matrix elements */
-class MatrixFlipper(flipper: Flipper[String]) extends Flipper[M] {
+/** @param cellFlipper object that flips matrix elements */
+class MatrixFlipper(cellFlipper: Flipper[String]) extends Flipper[M] {
 
   def flipLeftRight(matrix: M): M = {
     val rows = matrix.length
@@ -26,7 +26,7 @@ class MatrixFlipper(flipper: Flipper[String]) extends Flipper[M] {
     for {r <- 0 until rows
          c <- 0 until cols
     } {
-      result(r)(cols - 1 - c) = flipper.flipLeftRight(matrix(r)(c))
+      result(r)(cols - 1 - c) = cellFlipper.flipLeftRight(matrix(r)(c))
     }
     result
   }
@@ -38,7 +38,7 @@ class MatrixFlipper(flipper: Flipper[String]) extends Flipper[M] {
     for {r <- 0 until rows
          c <- 0 until cols
     } {
-      result(rows - 1 - r)(c) = flipper.flipBottomUp(matrix(r)(c))
+      result(rows - 1 - r)(c) = cellFlipper.flipBottomUp(matrix(r)(c))
     }
     result
   }
@@ -50,7 +50,7 @@ class MatrixFlipper(flipper: Flipper[String]) extends Flipper[M] {
     for {r <- 0 until rows
          c <- 0 until cols
     } {
-      result(c)(r) = flipper.flipNE2SW(matrix(r)(c))
+      result(c)(r) = cellFlipper.flipNE2SW(matrix(r)(c))
     }
     result
   }
@@ -62,7 +62,88 @@ class MatrixFlipper(flipper: Flipper[String]) extends Flipper[M] {
     for {r <- 0 until rows
          c <- 0 until cols
     } {
-      result(cols - 1 - c)(rows - 1 - r) = flipper.flipNW2SE(matrix(r)(c))
+      result(cols - 1 - c)(rows - 1 - r) = cellFlipper.flipNW2SE(matrix(r)(c))
+    }
+    result
+  }
+}
+
+object MatrixFlipper {
+
+  def flipDiamond(instruction: String, matrix: M) = {
+    instruction match {
+      case "x" => diamonds.flipNW2SE(matrix)
+      case "y" => diamonds.flipNE2SW(matrix)
+      case "r" => diamonds.flipNE2SW(diamonds.flipNW2SE(matrix))
+    }
+  }
+  def flipInterleaved(instruction: String, brickMatrix: M) = {
+    val matrix = fromBrickToCheckerboard(brickMatrix)
+    instruction match {
+      case "y" => interleaved.flipLeftRight(matrix)
+      case "x" => interleaved.flipBottomUp(matrix)
+      case "r" => interleaved.flipBottomUp(interleaved.flipLeftRight(matrix))
+    }
+  }
+  def flipBrick(instruction: String, brickMatrix: M) = {
+    val matrix = fromBrickToCheckerboard(brickMatrix)
+    instruction match {
+      case "y" => bricks.flipLeftRight(matrix)
+      case "x" => bricks.flipBottomUp(matrix)
+      case "r" => bricks.flipBottomUp(bricks.flipLeftRight(matrix))
+    }
+  }
+  val diamonds = new MatrixFlipper(ShortTupleFlipper)
+  val interleaved = new MatrixFlipper(LongTupleFlipper)
+  val bricks = new BrickFlipper(LongTupleFlipper)
+  
+  class BrickFlipper(cellFlipper: Flipper[String]) extends 
+       MatrixFlipper(cellFlipper: Flipper[String]) {
+  
+    // input | super | output
+    // ========================
+    // 1-2-3- | 3-2-1- | 3-2-1-
+    // -4-5-6 | -6-5-4 | -5-4-6
+    // 7-8-9- | 9-8-7- | 9-8-7-
+    // ==============================
+    // A-B-C-D- | D-C-B-A- | D-C-B-A-
+    // -E-F-G-H | -H-G-F-E | -G-F-E-H
+    // I-J-K-L- | L-K-J-I- | L-K-J-I-
+    // -M-N-O-P | -P-O-N-M | -O-N-M-P
+    // ==============================
+    override def flipLeftRight(matrix: M) = shiftOddRows(super.flipLeftRight(matrix))
+
+    // input | super | output
+    // ========================
+    // 1-2-3- | 7-8-9-
+    // -4-5-6 | -4-5-6
+    // 7-8-9- | 1-2-3-
+    // ==============================
+    // A-B-C-D- | M-N-O-P- | M-N-O-P-
+    // -E-F-G-H | -I-J-K-L | -J-K-L-I
+    // I-J-K-L- | E-F-G-H- | E-F-G-H-
+    // -M-N-O-P | -A-B-C-D | -B-C-D-A
+    // ==============================
+    override def flipBottomUp(matrix: M) = {
+
+      shiftOddRows(super.flipBottomUp(matrix))
+      if (matrix(0).length % 2 == 0)
+        shiftOddRows(matrix)
+      else matrix
+    }
+  }
+
+  private def shiftOddRows(matrix: M) = {
+
+    val result = matrix.map(_.clone())
+    val cols = matrix(0).length
+    val rows = matrix.length
+    for (r <- 1 until rows by 2) {
+      val saved = matrix(r)(0)
+      for (c <- 1 until cols) {
+        result(r)(c-1) =  matrix(r)(c)
+      }
+      result(r)(cols-1) = saved
     }
     result
   }
@@ -79,76 +160,4 @@ class MatrixFlipper(flipper: Flipper[String]) extends Flipper[M] {
     }
     result
   }
-}
-
-object MatrixFlipper {
-    def flipDiamond(instruction: String, matrix: M) = {
-        instruction match {
-        case "x" => diamonds.flipNW2SE(matrix)
-        case "y" => diamonds.flipNE2SW(matrix)
-        case "r" => diamonds.flipNE2SW(diamonds.flipNW2SE(matrix))
-        }
-    }
-    def flipInterleaved(instruction: String, brickMatrix: M) = {
-        val matrix = interleaved.fromBrickToCheckerboard(brickMatrix)
-        instruction match {
-        case "x" => interleaved.flipLeftRight(matrix)
-        case "y" => interleaved.flipBottomUp(matrix)
-        case "r" => interleaved.flipBottomUp(interleaved.flipLeftRight(matrix))
-        }
-    }
-    def flipBrick(instruction: String, brickMatrix: M) = {
-        val matrix = bricks.fromBrickToCheckerboard(brickMatrix)
-        instruction match {
-        case "x" => bricks.flipLeftRight(matrix)
-        case "y" => bricks.flipBottomUp(matrix)
-        case "r" => bricks.flipBottomUp(bricks.flipLeftRight(matrix))
-        }
-    }
-    private val diamonds = new MatrixFlipper(ShortTupleFlipper)
-    private val interleaved = new MatrixFlipper(LongTupleFlipper)
-    private val bricks = new MatrixFlipper(LongTupleFlipper) {
-    
-        // input | super | output
-        // ========================
-        // 1-2-3- | 3-2-1- | 3-2-1-
-        // -4-5-6 | -6-5-4 | -5-4-6
-        // 7-8-9- | 9-8-7- | 9-8-7-
-        // ==============================
-        // A-B-C-D- | D-C-B-A- | D-C-B-A-
-        // -E-F-G-H | -H-G-F-E | -G-F-E-H
-        // I-J-K-L- | L-K-J-I- | L-K-J-I-
-        // -M-N-O-P | -P-O-N-M | -O-N-M-P
-        // ==============================
-        override def flipLeftRight(matrix: M) = shiftOddRows(super.flipLeftRight(matrix))
-
-        // input | super | output
-        // ========================
-        // 1-2-3- | 7-8-9-
-        // -4-5-6 | -4-5-6
-        // 7-8-9- | 1-2-3-
-        // ==============================
-        // A-B-C-D- | M-N-O-P- | M-N-O-P-
-        // -E-F-G-H | -I-J-K-L | -J-K-L-I
-        // I-J-K-L- | E-F-G-H- | E-F-G-H-
-        // -M-N-O-P | -A-B-C-D | -B-C-D-A
-        // ==============================
-        override def flipBottomUp(matrix: M) = {
-
-          shiftOddRows(super.flipBottomUp(matrix))
-          if (matrix(0).length % 2 == 0)
-            shiftOddRows(matrix)
-          else matrix
-        }
-
-      private def shiftOddRows(matrix: M) = {
-
-        val result = matrix.map(_.clone())
-        val cols = matrix(0).length
-        for (r <- 1 to matrix.length by 2) {
-          result(r) = matrix(r).slice(1, cols - 1) ++ matrix(0)
-        }
-        result
-      }
-    }
 }
--- web/grounds/src/test/scala/dibl/FlipSpec.scala
+++ web/grounds/src/test/scala/dibl/FlipSpec.scala
@@ -17,12 +17,63 @@
 package dibl
 
 import org.scalatest._
+import dibl.MatrixFlipper._
+import dibl.Graphs._
 
 class FlipSpec extends FlatSpec with Matchers {
+
+  val m3x3 = M(R("1", "2", "3"), 
+               R("4", "5", "6"), 
+               R("7", "8", "9"))
+  val m4x4 = M(R("A", "B", "C", "D"), 
+               R("E", "F", "G", "H"), 
+               R("I", "J", "K", "L"), 
+               R("M", "N", "O", "P"))
+  val m4x2 = M(R("A", "B"), 
+               R("C", "D"), 
+               R("E", "F"), 
+               R("G", "H"))
+  val m2x4 = M(R("A", "B", "C", "D"), 
+               R("E", "F", "G", "H"))
+               
+  val brickFlipper = new BrickFlipper(new Flipper[String] {
+    def flipLeftRight(o: String): String = o
+    def flipBottomUp(o: String): String = o
+    def flipNW2SE(o: String): String = o
+    def flipNE2SW(o: String): String = o
+  })
+  
+  "y 3x3" should "succeed" in {
+    brickFlipper.flipLeftRight(fromBrickToCheckerboard(m3x3)) shouldBe
+      M(R("3", "2", "1"), 
+        R("5", "4", "6"), 
+        R("9", "8", "7"))
+  }
+  "x 3x3" should "succeed" in {
+    brickFlipper.flipBottomUp(fromBrickToCheckerboard(m3x3)) shouldBe 
+      M(R("7", "8", "9"), 
+        R("4", "5", "6"), 
+        R("1", "2", "3"))
+  }
+  "y 4x4" should "succeed" in {
+    brickFlipper.flipLeftRight(fromBrickToCheckerboard(m4x4)) shouldBe
+      M(R("D", "C", "B", "A"),
+        R("G", "F", "E", "H"), 
+        R("L", "K", "J", "I"), 
+        R("O", "N", "M", "P"))
+  }
+  "x 4x4" should "succeed" in {
+    brickFlipper.flipBottomUp(fromBrickToCheckerboard(m4x4)) shouldBe
+      M(R("M", "N", "O", "P"),
+        R("J", "K", "L", "I"),
+        R("E", "F", "G", "H"), 
+        R("B", "C", "D", "A"))
+  }
+  
   "flip back and forth" should "render original" in {
     val m1 = Graphs.get("interleaved-4x2-pair",0,M(R("")))
-    val flipped: M = MatrixFlipper.flipInterleaved("x", m1)
-    val m2 = MatrixFlipper.flipInterleaved("x",flipped)
+    val flipped: M = flipInterleaved("x", m1)
+    val m2 = flipInterleaved("x",flipped)
     for {
       r <- m1.indices
       c <- m1(0).indices
